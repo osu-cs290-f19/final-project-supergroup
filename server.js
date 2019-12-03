5d0248d946f0ababbe45d2f6522278a02063fcec
nozoko <shulerp@oregonstate.edu>
2019-12-03 15:35:52 -0800

    Added browse and post template pages, and made sure links worked properly

--- server.js
+++ server.js
@@ -5,18 +5,43 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
-// add JSON file that stores posts
+var fs = require('fs');
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//Parse JSON file
+var rawJson = fs.readFileSync('./userPosts.json', 'utf8');
+var postsArray = JSON.parse(rawJson);
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.status('200');
     res.render('home');
+});
+
+app.get('/browse', function (req, res) {
+    res.status('200');
+    res.render('browse', {
+		showsearch: true, 
+		post: postsArray
+		});
+});
+
+app.get('/posts/:postNum', function (req, res, next) {
+	var postNum = req.params.postNum;
+	if (+postNum >= 0 && +postNum < +postsArray.length) {
+		res.render('browse', {
+		showsearch: false,
+		post: [postsArray[postNum]]
+	});
+	}
+	else {
+		next();
+	}
 });
 
 // add any other redirects
