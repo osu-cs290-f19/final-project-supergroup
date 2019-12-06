15d4e42443d38ffaa32ad1ab63833247ab75aa15
Peter Bloch <blochp@10-251-21-32.wireless.oregonstate.edu>
2019-12-06 13:09:16 -0800

    check posts for filters

--- public/functionHandler.js
+++ public/functionHandler.js
@@ -5,16 +5,47 @@ console.log('loading index.js');
 var contentArray= [];
 
 function insertPost(){
-    
+    //
 }
 
 function checkFilters(post, filters){
-    //check title
+    //check title and body
+    if (filters.text){
+        var post_title = post.title.toLowerCase();
+        var post_body = post.body.toLowerCase();
+        var filter_text = filters.text.toLowerCase();
+        if (post_title.indexOf(filter_text)<0 && post_body.indexOf(filter_text)<0){
+            return false;
+        }
+    }
     //check class
+    if (filters.class){
+        var filter_class = filters.class.toLowerCase();
+        var post_class = post.class.toLowerCase()
+        if(post_class.indexOf(filter_class)<0){
+            return false;
+        }
+    }
     //check term
-    //check upload date
+    if (filters.term){
+        var filter_term = filters.term.toLowerCase();
+        var post_term = post.term.toLowerCase();
+        if(post_term.indexOf(filter_term)<0){
+            return false;
+        }
+    }
+    //check upload date?
     //check professor
-    //check body
+    if (filters.professor){
+        var filter_prof = filters.professor.toLowerCase();
+        var post_prof = post.professor.toLowerCase();
+        if(post_prof.indexOf(filter_prof)<0){
+            return false;
+        }
+    }
+
+    //if passes all tests...
+    return true;
 }
 
 function updatePosts(event){
