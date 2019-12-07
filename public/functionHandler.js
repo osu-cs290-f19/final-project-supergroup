7c89efed5f4d2d8f4c80403fa62dfa3493229ca3
Peter Bloch <blochp@10-251-21-32.wireless.oregonstate.edu>
2019-12-07 14:29:20 -0800

    recognizes original content

--- public/functionHandler.js
+++ public/functionHandler.js
@@ -3,9 +3,10 @@
 console.log('loading index.js');
 var contentArray= [];
 
-function create_posts(){
-    //nada for now
+function grabData(){
+    contentArray = document.getElementsByClassName("user-post");
 }
+
 
 function checkFilters(post, filters){
     //check title
@@ -40,15 +41,23 @@ function checkFilters(post, filters){
             return false;
         }
     }
+    if (filters.year){
+        var filter_year = filters.year.toLowerCase();
+        var post_year = post.year.toLowerCase();
+        if (post_year.indexOf(filter_year)<0){
+            return false;
+        }
+    }
 
     //if passes all tests...
     return true;
 }
 
 function delete_posts(){
-    var postsArray = document.getElementsByClassName("userPost");
-    console.log(postsArray);
+    var postsArray = document.querySelectorAll("post-container");
+    console.log("posts array lastchild:",postsArray.lastChild);
     while (postsArray.lastChild){
+        //contentArray.push(postsArray.lastChild);
         postsArray.removeChild(postsArray.lastChild);
     }
 }
@@ -56,34 +65,30 @@ function delete_posts(){
 function updatePosts(event){
     //delete all posts in dom
     delete_posts();
-    //re-create all posts from back end data.
-    create_posts();
+    //re-create all posts from back end data and
     //update posts to match search parameters
     var new_filters = {
     class: document.getElementById('class-search').value,
     professor: document.getElementById('professor-search').value,
     text: document.getElementById('search-bar-text').value,
-    term: document.getElementById("select-term").value
+    term: document.getElementById("select-term").value,
+    year: document.getElementById("year-input").value
     }
-    post_info = document.querySelectorAll(".user-post");
+    var original_posts = contentArray;
     //console.log(post_info.length);
-    for (var i =post_info.length-1; i>=0;i--){ 
+    for (var i =original_posts.length-1; i>=0;i--){ 
         var individual_post = {
-        professor: post_info[i].getAttribute('data-professor'),
-        class: post_info[i].getAttribute('data-class'),
-        term: post_info[i].getAttribute('data-term'),
-        title: post_info[i].getElementsByClassName('post-title')[0].textContent
+        professor: original_posts[i].getAttribute('data-professor'),
+        class: original_posts[i].getAttribute('data-class'),
+        term: original_posts[i].getAttribute('data-term'),
+        title: original_posts[i].getElementsByClassName('post-title')[0].textContent
+        //year: original_posts[i].getAttribute('data-year')
         }
         if (!checkFilters(individual_post,new_filters)){
             console.log("failed test");
         }else{
             console.log("passed test");
         }
-        //console.log(post_class,post_prof);
-        //var post_year = post_info[i].getAttribute('data-year');
-        //var post_term = post_info[i].getAttribute('data-term');
-
-        //check all variables
     }
 }
 
@@ -91,3 +96,5 @@ function updatePosts(event){
 var update_button = document.getElementById('search-button');
 console.log('found search button:\n',update_button);
 update_button.addEventListener('click',updatePosts);
+window.addEventListener('load',grabData());
+console.log(contentArray);
